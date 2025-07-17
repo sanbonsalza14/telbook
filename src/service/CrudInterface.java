2cf75821abd3ed010136170f009e077de633cfb7
unknown <slamguy14@naver.com>
2025-07-17 13:21:19 +0900

    getListAll

---
+++ src/service/CrudInterface.java
@@ -0,0 +1,18 @@
+﻿package service;
+
+import dto.TelDto;
+
+import java.util.List;
+
+public interface CrudInterface {
+    int InsertData(TelDto dto);
+    int UpdateData(TelDto dto);
+
+    int deleteData(int id);
+
+    List<TelDto> getListAll();// 전체 찾기
+
+    TelDto findbyId(int id);//한 개 테이터 찾기
+
+    List<TelDto> searcHList(String keyword); //이름검색
+}
